9e49c33513db4e8fe747f5ecf0d0fca29b9b9e1e
andresprogramacion123 <julian.montoya2@udea.edu.co>
2024-11-10 23:08:13 -0500

    mensaje de bienvenida

--- app/main.py
+++ app/main.py
@@ -3,4 +3,9 @@ from app.routers import fondo
 
 app = FastAPI()
 
+@app.get("/",tags=["Bienvenida"])
+def bienvenido():
+    return {"Mensaje":"Bienvenido a la API de BTG Pactual"}
+
+
 app.include_router(fondo.router)
